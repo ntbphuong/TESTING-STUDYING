ec8694846ed84251c640fb0f833c995cc406fe0a
BinhPhuong <ntbphuong17017@gmail.com>
2026-05-22 21:40:28 +0700

    update tinh nang

---
+++ Src/loginStore.js
@@ -0,0 +1,14 @@
+function login(emailInput, passwordInput){
+    //1.Kiem tra email
+    if(!emailInput) {
+        return "Vui long nhap email"
+    }
+    //1.Kiem tra email
+    if(!emailInput) {
+        return "Vui long nhap email"
+    }
+    //1.Kiem tra email
+    if(!emailInput) {
+        return "Vui long nhap email"
+    }
+}
