be9b1645a2e53fda9b4ae8cc99eb10f1e2c17127
BinhPhuong <ntbphuong17017@gmail.com>
2026-05-25 15:54:08 +0700

    update css thongbao

--- Src/loginStore.js
+++ Src/loginStore.js
@@ -1,14 +1,17 @@
 function login(emailInput, passwordInput){
-    //1.Kiem tra email
+    // Kiem tra email
     if(!emailInput) {
         return "Vui long nhap email"
     }
-    //1.Kiem tra email
-    if(!emailInput) {
-        return "Vui long nhap email"
+    // Kiem tra mật khẩu
+    if(!passwordInput) {
+        return "Vui long nhap mật khẩu"
     }
-    //1.Kiem tra email
-    if(!emailInput) {
-        return "Vui long nhap email"
+    // Kiem tra tài khoản có đúng không
+    // Giả sử: admin@gmail.com và 123456 là đúng
+    if(!emailInput == "admin@gmail.com" && passwordInput =="123456") {
+        return "Đăng nhập thành công!";
+    } else{
+        return "Đăng nhập thất bại";
     }
 }
